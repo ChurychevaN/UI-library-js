7e2698d4347b3d6fa612850010d4f02d4b385f15
Your Name <you@example.com>
2022-01-26 18:11:40 +0100

    lesson-39

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,15 @@ import './lib/lib';
 
 //$('div').hide.show();
 $('button').on('click', function(){
-	$(this).toggleClass('active');
+	$('div').eq(1).toggleClass('active');
 });
 
+$('div').click(function() {
+	console.log($(this).index());
+});
+
+//console.log($('div').eq(2).find('.some'));
+//console.log($('.some').closest('.findme'));
+console.log($('.more').eq(0).siblings());
+
+//console.log($('button').html());
