20216a91c4e099037426653c10215674d3f7c280
Your Name <you@example.com>
2022-02-06 18:00:23 +0100

    lesson-46

--- src/js/main.js
+++ src/js/main.js
@@ -13,17 +13,38 @@ $('button').eq(2).on('click', () => {
 	$('.w-500').fadeToggle(800);
 });
 
-$('.wrap').html(
-	`
- 	<div class="dropdown">
-            <button class="btn btn-primery dropdown-toggle" id="dropdownMenuButton">Dropdown
-                button</button>
-            <div class="dropdown-menu" data-toggel-id="dropdownMenuButton">
-                <a href="" class="dropdown-item">Action</a>
-                <a href="" class="dropdown-item">Action #2</a>
-                <a href="" class="dropdown-item">Action #3</a>
-            </div>
- 	</div>`
-);
+$('#trigger').click(() => $('#trigger').createModal({
+	text: {
+		title: 'Modal title',
+		body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+			' Aliquid consectetur cum doloribus error eveniet explicabo incidunt ipsum' +
+			' laboriosam laborum molestiae nihil officiis perferendis perspiciatis possimus praesentium quas repellat, sequi sit?'
+	},
+	btns: {
+		count: 3,
+		settings: [
+			[
+				'Close',
+				['btn-danger', 'mr-10'],
+				true
+			],
+			[
+				'Save changes',
+				['btn-success'],
+				false,
+				() => {
+					alert('Data saved');
+				}
+			],
+			[
+				'Another btn',
+				['btn-warning', 'ml-10'],
+				false,
+				() => {
+			alert('Hello World');
+				}
+			]
+		]
+	}
+}));
 
-$('.dropdown-toggle').dropdown();
